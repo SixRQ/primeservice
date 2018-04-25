43dc9ab42ae09c56254f2c89e5b48cd07205a83d
Simon Wiehe <simon.wiehe@sixrq.com>
2018-04-25 11:24:09 +0100

    Add SpringBoot to the gradle project and basic main class

---
+++ src/main/java/com/sixrq/primeservice/Service.java
@@ -0,0 +1,12 @@
+package com.sixrq.primeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Service.class, args);
+    }
+}
